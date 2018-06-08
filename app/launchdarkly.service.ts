c056176a5e40a7678f7245b12e0ed1b3c7020d6e
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2018-06-08 14:57:22 -0700

    Update launchdarkly.service.ts

--- app/launchdarkly.service.ts
+++ app/launchdarkly.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
-import * as LDClient from 'ldclient-js';
+import { LDClient } from 'ldclient-js';
 
 @Injectable()
 export class LaunchDarklyService {
